7c9900cf403c0574ec20c89697614aa292f32225
baegteun <baegteun@gmail.com>
2022-09-19 14:44:48 +0900

    Initial Commit

---
+++ WDE Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WDE Watch App
+//
+//  Created by 최형우 on 2022/09/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WDE Watch App/WDEApp.swift
@@ -0,0 +1,17 @@
+//
+//  WDEApp.swift
+//  WDE Watch App
+//
+//  Created by 최형우 on 2022/09/19.
+//
+
+import SwiftUI
+
+@main
+struct WDE_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
